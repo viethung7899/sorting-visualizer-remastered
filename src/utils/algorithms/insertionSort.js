6b8230af698019b0d3f1100725dbc1d6f91055e5
viethung7899 <viethung07081999@gmail.com>
2021-01-23 05:49:47 -0800

    Fix animation for insertion sort

--- src/utils/algorithms/insertionSort.js
+++ src/utils/algorithms/insertionSort.js
@@ -5,11 +5,14 @@ import { swapArray } from '../utils';
 const insertionSort = (array) => {
   const actions = [];
   for (let i = 1; i < array.length; i++) {
+    actions.push(mark(STATUS.RESERVE, i));
     for (let j = i; j > 0 && array[j - 1] > array[j]; j--) {
       actions.push(mark(STATUS.SELECTED, j - 1, j), wait);
       actions.push(swap(j - 1, j));
       swapArray(array, j - 1, j);
-      actions.push(unmark(j - 1, j));
+      if (j == i) actions.push(mark(STATUS.RESERVE, i));
+      else actions.push(unmark(j));
+      actions.push(unmark(j - 1), wait);
     }
   }
 
